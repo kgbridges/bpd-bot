6566a0f061ecb326868c2a78bc01f7cc9e2542d9
Katie Bridges <kgrace.bridges@gmail.com>
2022-02-06 13:04:59 -0700

    Update create_dataframe.py

--- create_dataframe.py
+++ create_dataframe.py
@@ -2,7 +2,6 @@ import requests
 import pandas as pd
 
 # Pull data from ArcGIS API
-
 url = "https://services.arcgis.com/ePKBjXrBZ2vEEgWd/arcgis/rest/services/BPD_Offenses/FeatureServer/0/query?where=1%3D1&outFields=Report_Number,Report_Date&returnGeometry=false&returnCountOnly=false&outSR=4326&f=json"
 
 payload = {}
